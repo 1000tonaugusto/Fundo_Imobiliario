76d53fca366d8292f01493911f0bb3d42c8bb421
1000tonaugusto <miltonaugustodesouza@gmail.com>
2024-03-25 16:56:54 -0300

    Ajuste deploy 006

--- core/settings.py
+++ core/settings.py
@@ -148,3 +148,7 @@ DECIMAL_SEPARATOR = '.'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR,'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
